a89398ef79d2252d2df66da91d9470d798a487da
ZXM-MD <110195953+ZXM-MD@users.noreply.github.com>
2022-12-27 20:15:28 +0700

    Add files via upload

--- plugins/main-menu-1.js
+++ plugins/main-menu-1.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 import fs from 'fs'
 let handler = async (m, { conn, args, command }) => {
-  const ultah = new Date('October 21 2023 00:0:01')
+  const ultah = new Date('Agustus 08 2023 00:0:01')
     const sekarat = new Date().getTime() 
     const Kurang = ultah - sekarat
     const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
@@ -13,13 +13,13 @@ let totalf = Object.values(global.plugins).filter(
   ).length;
  await conn.sendButton(m.chat, `*Hai Banh*
 Saya Adalah Khoiyrul Botz Yang Dibuat Oleh Pak Zainal Dev's Pada Tanggal 16 12 2022.
-Kalau Ada Yg Error Bisa Langsung Report Ke Owner.\n`,wm + '\n\n' + botdate, thumbdoc, [['Aʟʟ Mᴇɴᴜ','.? all'],['Lɪsᴛ Mᴇɴᴜ','.icikiwir']], m, {
+Kalau Ada Yang Error Bisa Langsung Report Ke Owner.\n`,wm + '\n\n' + botdate, thumbdoc, [['Aʟʟ Mᴇɴᴜ','.? all'],['Lɪsᴛ Mᴇɴᴜ','.icikiwir']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
                         title: `Ultah Pak Zainal Dev's🍀 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
-                        body: `Join Grup`,          previewType: 0,
+                        body: `PAK ZAINAL DEV'S`,          previewType: 0,
                         thumbnail: await (await fetch(`https://telegra.ph/file/ba25e84b7335e2f14de67.jpg`)).buffer(),
                         sourceUrl: 'https://youtube.com/@hagozox'
                       }}
