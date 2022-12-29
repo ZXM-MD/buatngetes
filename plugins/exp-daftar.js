4533e59e375511557a6190bf520a0d8755a93ba2
ZXM-MD <110195953+ZXM-MD@users.noreply.github.com>
2022-12-29 18:35:37 +0800

    Update exp-daftar.js

--- plugins/exp-daftar.js
+++ plugins/exp-daftar.js
@@ -9,29 +9,29 @@ let handler = async function (m, { text, usedPrefix, command }) {
 	let namae = conn.getName(m.sender)
 	const sections = [
 	{
-	title: "- - - - 『 ▮ 📆 Pilih Umur Kamu Disini !」 』 - - - -",
+	title: "📆Pilih Umur Kamu Disini !",
 	rows: [
-	    {title: "📛 )ഒ Random Tahun", rowId: '.daftar ' + namae + '.' + pickRandom(['30','20','18','16','13']), description: "✧ Kira² beliau ini mendapatkan umur berapa ya 💀"},
+	    {title: "📛Random Tahun", rowId: '.daftar ' + namae + '.' + pickRandom(['30','20','18','16','13',])}
 	]
     },
     {
-	title: "- - - - 『 ▮ 🎲 T U A 」 』 - - - -",
+	title: "🎲T U A",
 	rows: [
-	    {title: "😝 〉ɞ 『 Kuliah 』", rowId: '.daftar ' + namae + '.20 ', description: "✧ Wah udah kerja nih 😅"},
-	    {title: "😑 〉ɞ 『 Pengangguran 』", rowId: '.daftar ' + namae + '.18 ', description: "✧ Wah ada beban keluarga nih 🙃"},
-	    {title: "😥 〉ɞ 『 Om Om 』", rowId: '.daftar ' + namae + '.30 ', description: "✧ Om halalin aku donh:v 🤭"},
+	    {title: "😝Kuliah", rowId: '.daftar ' + namae + '.20 '},
+	    {title: "😑Pengangguran", rowId: '.daftar ' + namae + '.18 '},
+	    {title: "😥Om Om", rowId: '.daftar ' + namae + '.30 '},
 	]
     },
     {
-	title: "- - - - 『 ▮ 🥊 B O C A H 」 』 - - - -",
+	title: "🥊B O C A H",
 	rows: [
-	    {title: "🗿Sma", rowId: '.daftar ' + namae + '.16 ', description: "✧ Saya masih Sma kak 🙄"},
-	    {title: "😱Smk", rowId: '.daftar ' + namae + '.16 ', description: "✧ Saya masih Smk kak 😒"
-	    {title: "🤡Esempeh", rowId: '.daftar ' + namae + '.13 ', description: "✧ Saya masih Smp kak 😏"
-	{title: "🤪Emtees", rowId: '.daftar ' + namae + '.13 ', description: "✧ Saya masih Mts kak 🥶"
-	{title: "🤓Esdeh", rowId: '.daftar ' + namae + '.5 ', description: "✧ Saya masih Sd kak 😋"
-	{title: "😜Teka", rowId: '.daftar ' + namae + '.4 ', description: "✧ Saya masih Tk kak 🤪"
-	{title: "😐Paud", rowId: '.daftar ' + namae + '.4 ', description: "✧ Saya masih Paud kak 🥴"
+	    {title: "🗿Sma", rowId: '.daftar ' + namae + '.16 '},
+	    {title: "😱Smk", rowId: '.daftar ' + namae + '.16 '},
+	    {title: "🤡Esempeh", rowId: '.daftar ' + namae + '.13 '},
+	{title: "🤪Emtees", rowId: '.daftar ' + namae + '.13 '},
+	{title: "🤓Esdeh", rowId: '.daftar ' + namae + '.5 '},
+	{title: "😜Teka", rowId: '.daftar ' + namae + '.4 '},
+	{title: "😐Paud", rowId: '.daftar ' + namae + '.4 '},
 	]
 	},
 ]
