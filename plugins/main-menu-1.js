fb493521dd462371bbe1a5d002d19510f7528415
ZXM-MD <110195953+ZXM-MD@users.noreply.github.com>
2023-01-05 02:16:44 +0800

    Update main-menu-1.js

--- plugins/main-menu-1.js
+++ plugins/main-menu-1.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 import fs from 'fs'
 let handler = async (m, { conn, args, command }) => {
-  const ultah = new Date('Agustus 08 2023 00:0:01')
+  const ultah = new Date('August 08 2023 00:0:01')
     const sekarat = new Date().getTime() 
     const Kurang = ultah - sekarat
     const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
